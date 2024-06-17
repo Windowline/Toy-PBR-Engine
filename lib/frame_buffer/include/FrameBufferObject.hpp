f562becec08537e408cbc964131914a1f9fb8b6c
박창선 <>
2024-06-17 19:48:37 +0900

    start

---
+++ lib/frame_buffer/include/FrameBufferObject.hpp
@@ -0,0 +1,57 @@
+#include <memory>
+//#include <OpenGLES/ES3/gl.h>
+#include <glad/glad.h>
+#include "Vector.hpp"
+
+/*
+ * off screen에 결과물을 그려두기 위한 FrameBufferObject입니다.
+ */
+class FrameBufferObject {
+public:
+    
+    enum class Type : int {
+        GBuffer = 0,
+        Common = 1
+    };
+    
+    
+    FrameBufferObject(ivec2 size, GLuint defaultFbo, Type type);
+    ~FrameBufferObject();
+
+    ivec2 size() const;
+    void bindWithViewport();
+    GLuint texture() const;
+    
+    GLuint gPositionTexture() const {
+        return _gPosition;
+    }
+    
+    GLuint gNormalTexture() const {
+        return _gNormal;
+    }
+    
+    GLuint gAlbedoTexture() const {
+        return _gAlbedo;
+    }
+    
+    GLuint commonTexture() const {
+        return _commonTexture;
+    }
+
+private:
+    void init();
+
+private:
+    Type _type;
+    unsigned int _defaultFbo;
+    ivec2 _size;
+    unsigned int _fboId;
+
+    unsigned int _gPosition;
+    unsigned int _gNormal;
+    unsigned int _gAlbedo;
+    unsigned int _commonTexture;
+
+    unsigned int _rboDepth;
+};
+
---
+++ lib/shader/src/ShaderManager.cpp
@@ -0,0 +1,104 @@
+
+#include "../include/ShaderManager.hpp"
+#include "../include/ShaderBase.hpp"
+#include "../include/BasicLightingShader.hpp"
+#include "../include/GBufferShader.hpp"
+#include "../include/TexturePassShader.hpp"
+#include "../include/DeferredLightingShader.hpp"
+#include "../include/ShadowDepthShader.hpp"
+#include "../include/SSAOShader.hpp"
+#include "../include/SSAOBlurShader.hpp"
+#include "../include/AlbedoColorShader.hpp"
+
+
+ShaderManager::ShaderManager()
+    : _activeShader(nullptr) {
+}
+
+void ShaderManager::addShader(int shaderId, std::shared_ptr<ShaderBase> const & shaderProgram ) {
+    auto it = _shaderMap.find(shaderId);
+
+    if(it == _shaderMap.end()) {
+        _shaderMap.insert({shaderId, shaderProgram});
+    }
+}
+
+void ShaderManager::removeShader(int shaderId ) {
+    _shaderMap.erase(shaderId);
+}
+
+std::shared_ptr<ShaderBase> ShaderManager::findShader(int shaderId) const {
+    auto it = _shaderMap.find(shaderId);
+
+    if (it != _shaderMap.end()) {
+        if (!it->second->isLoaded()) {
+            it->second->load();
+        }
+        return it->second;
+    }
+
+    return nullptr;
+}
+
+
+ShaderManager::~ShaderManager() {
+    unloadAllPrograms();
+    _shaderMap.clear();
+}
+
+void ShaderManager::loadAllPrograms() {
+    
+    if (_shaderMap.empty()) {
+        _shaderMap = {
+            {eShaderProgram_Default,        std::make_shared<BasicLightingShader>()},
+            {eShaderProgram_GBuffer,        std::make_shared<GBufferShader>()},
+            {eShaderProgram_DeferredLighting, std::make_shared<DeferredLightingShader>()},
+            {eShaderProgram_TexturePass,    std::make_shared<TexturePassShader>() },
+            {eShaderProgram_ShadowDepth,    std::make_shared<ShadowDepthShader>()},
+            {eShaderProgram_SSAO,           std::make_shared<SSAOShader>()},
+            {eShaderProgram_SSAO_BLUR,      std::make_shared<SSAOBlurShader>()},
+            {eShaderProgram_ALBEDO_COLOR,   std::make_shared<AlbedoColorShader>()}
+        };
+    }
+    
+    checkGLError();
+}
+
+void ShaderManager::checkGLError() {
+    GLenum ret = glGetError();
+    if(ret != GL_NO_ERROR) {
+        printf(">> GL_ERROR: 0x%04x \n", ret);
+    }
+}
+
+void ShaderManager::unloadAllPrograms() {
+    for(auto it = _shaderMap.begin(); it != _shaderMap.end(); it++) {
+        it->second->unload();
+    }
+}
+
+std::shared_ptr<ShaderBase> const & ShaderManager::getActiveShader() const {
+    return _activeShader;
+}
+
+
+void ShaderManager::setActiveShader(std::shared_ptr<ShaderBase> const & shader) {
+    if( _activeShader == shader || shader == nullptr )
+        return;
+    
+    if (!shader->isLoaded()) {
+        shader->load();
+    }
+    _activeShader = shader;
+    _activeShader->useProgram();
+
+    auto & attributes = _activeShader->getEnabledAttributes();
+    for( int i = 0 ; i < VertexAttribute_MAX ; ++i) {
+        if(attributes[i])
+            glEnableVertexAttribArray(i);
+        else
+            glDisableVertexAttribArray(i);
+    }
+
+    return;
+}
